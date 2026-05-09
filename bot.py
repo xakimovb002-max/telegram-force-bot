1bdd1253b158a4d2439aad8b0c03718fa39b3571
xakimovb002-max <xakimovb002@gmail.com>
2026-05-10 00:38:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,34 +7,29 @@ TOKEN = "8645242729:AAELpQmB6-Kydw6lz6JJN11ScRUh5tAjeoQ"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# MENU
 menu = ReplyKeyboardMarkup(resize_keyboard=True)
-button1 = KeyboardButton("💵 Valyuta kursi")
-menu.add(button1)
+menu.add(KeyboardButton("💵 Valyuta kursi"))
 
-# START
 @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
+async def start(message: types.Message):
     await message.answer(
-        "✅ Menu ishladi",
+        "✅ Bot ishladi",
         reply_markup=menu
     )
 
-# VALYUTA
 @dp.message_handler()
-async def messages(message: types.Message):
+async def handler(message: types.Message):
 
     if message.text == "💵 Valyuta kursi":
 
-        try:
-            url = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/"
-            data = requests.get(url).json()
+        url = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/"
+        data = requests.get(url).json()
 
-            usd = next(item for item in data if item["Ccy"] == "USD")
-            eur = next(item for item in data if item["Ccy"] == "EUR")
-            rub = next(item for item in data if item["Ccy"] == "RUB")
+        usd = next(x for x in data if x["Ccy"] == "USD")
+        eur = next(x for x in data if x["Ccy"] == "EUR")
+        rub = next(x for x in data if x["Ccy"] == "RUB")
 
-            text = f"""
+        text = f"""
 💵 Valyuta kurslari
 
 🇺🇸 USD: {usd['Rate']} so'm
@@ -42,12 +37,8 @@ async def messages(message: types.Message):
 🇷🇺 RUB: {rub['Rate']} so'm
 """
 
-            await message.answer(text)
+        await message.answer(text)
 
-        except Exception as e:
-            await message.answer(f"Xato: {e}")
-
-# RUN
 if __name__ == "__main__":
     print("Bot ishga tushdi")
     executor.start_polling(dp, skip_updates=True)
